4f1c89e8d39d5caa6d32223c067062998517e850
gudals123 <104682516+gudals123@users.noreply.github.com>
2024-05-09 06:44:39 +0900

    카메라 전환 기능 추가

---
+++ P20240503_Altang/Assets/Viewpointer_Transformation/Script/ViewPointerTransformation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class ViewPointerTransformation : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<GameObject> Cameras;
+
+    private int runningCamera = 0;
+    private int cameraCount;
+
+    void Start()
+    {
+        foreach (var cam in Cameras)
+        {
+            cam.SetActive(false);
+        }
+        Cameras[runningCamera].SetActive(true);
+        cameraCount = Cameras.Count;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Cameras[runningCamera].SetActive(false);
+
+            ++runningCamera;
+            runningCamera = runningCamera % cameraCount;
+
+            Cameras[runningCamera].SetActive(true);
+        }
+    }
+}
